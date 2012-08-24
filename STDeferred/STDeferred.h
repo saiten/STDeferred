846c7d30edf2266aa14d496ccbc480a16282624e
saiten <saiten@isidesystem.net>
2012-08-24 22:51:51 +0900

    Initial Commit

---
+++ STDeferred/STDeferred.h
@@ -0,0 +1,13 @@
+//
+//  STDeferred.h
+//  STDeferred
+//
+//  Created by saiten on 12/08/24.
+//  Copyright (c) 2012年 saiten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STDeferred : NSObject
+
+@end
---
+++ STDeferred/STDeferred.m
@@ -0,0 +1,13 @@
+//
+//  STDeferred.m
+//  STDeferred
+//
+//  Created by saiten on 12/08/24.
+//  Copyright (c) 2012年 saiten. All rights reserved.
+//
+
+#import "STDeferred.h"
+
+@implementation STDeferred
+
+@end
