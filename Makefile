9d5eb784b227c9e48de3f7516b9c45be0a079258
saiten <saiten@isidesystem.net>
2013-05-25 18:51:34 +0900

    fixed test environment

--- Makefile
+++ Makefile
@@ -5,6 +5,5 @@ test:
 		-scheme STDeferredTest \
 		-configuration Debug \
 		clean build \
-		GHUNIT_CLI=1 \
-		ONLY_ACTIVE_ARCH=NO \
-		TEST_AFTER_BUILD=YES
+	GHUNIT_CLI=1 \
+	ONLY_ACTIVE_ARCH=NO \
--- STDeferredTest/main.m
+++ STDeferredTest/main.m
@@ -7,10 +7,38 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GHUnitIOS/GHUnit.h>
+
+@interface MyUIApp : UIApplication
+@end
+
+@implementation MyUIApp
+
+- (id)init
+{
+    self = [super init];
+    if (self && getenv("GHUNIT_CLI") && [[[UIDevice currentDevice] systemVersion] doubleValue] >= 5.0) {
+        
+        __block BOOL done = NO;
+        NSOperationQueue * queue = [[NSOperationQueue alloc ] init];
+        [queue addOperationWithBlock:^{
+            [GHTestRunner run];
+            done = YES;
+        }];
+        
+        while( !done ) {
+            [[NSRunLoop currentRunLoop] runUntilDate: [NSDate dateWithTimeIntervalSinceNow:5]];
+        }
+    }
+    
+    return self;
+}
+
+@end
 
 int main(int argc, char *argv[])
 {
   @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+      return UIApplicationMain(argc, argv, @"MyUIApp", @"GHUnitIOSAppDelegate");
   }
 }
